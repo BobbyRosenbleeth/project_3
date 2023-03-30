431ab2ac215e7b3dcfe28798ae3dc6f285f5b084
sraven24 <ksanders236@gmail.com>
2023-03-29 20:14:28 -0400

    pull html using jquery

--- ufo_scrape.js
+++ ufo_scrape.js
@@ -1,10 +1,16 @@
-// get the data endpoints
-const jan23 = "https://nuforc.org/webreports/ndxe202301.html"
-const feb23 = "https://nuforc.org/webreports/ndxe202302.html"
-const mar23 = "https://nuforc.org/webreports/ndxe202303.html"
+// // get the data endpoints
+ //const jan23 = "https://nuforc.org/webreports/ndxe202301.html"
+// const feb23 = "https://nuforc.org/webreports/ndxe202302.html"
+// const mar23 = "https://nuforc.org/webreports/ndxe202303.html"
 
-//jsonify each page
 
-const jan = d3.json(jan23);
-console.log(jan);
+$.get('https://nuforc.org/webreports/ndxe202301.html', function(my_var) {
+    console.log(my_var);
+});
+
+
+
+
+
+
 
