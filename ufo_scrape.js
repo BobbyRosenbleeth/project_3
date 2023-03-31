390cb376c68d00b813d3af43f8b13aaffc9ab663
sraven24 <ksanders236@gmail.com>
2023-03-31 12:11:29 -0400

    retrieved data from sites and converted them into JSON objects

--- ufo_scrape.js
+++ ufo_scrape.js
@@ -1,44 +1,93 @@
 // // get the data endpoints
- //const jan23 = "https://nuforc.org/webreports/ndxe202301.html"
+//const jan23 = "https://nuforc.org/webreports/ndxe202301.html"
 // const feb23 = "https://nuforc.org/webreports/ndxe202302.html"
 // const mar23 = "https://nuforc.org/webreports/ndxe202303.html"
 
-
+//get the data from the site for january 2023
 $.get('https://nuforc.org/webreports/ndxe202301.html', function(jan23) {
-    // const parser = new DOMParser();
-    // const parsedjan = parser.parseFromString(jan23, 'text/html');
-    // var allText = parsedjan.all[0].textContent;
-    // console.log(allText);
-    var myRows = { myRows: [] };
+      //create an array to store the table data
+         const tableData1 = [];
+    //iterate through the table data by each row and separate out the td 
+         $(jan23).find('tr').each(function(row, tr) {
+           
+          tableData1[row] = {
+            "Date": $(tr).find('TD:eq(0)').text(),
+            "City": $(tr).find('TD:eq(1)').text(),
+            "State": $(tr).find('TD:eq(2)').text(),
+            "County": $(tr).find('TD:eq(3)').text(),
+            "Shape": $(tr).find('TD:eq(4)').text(),
+            "Duration": $(tr).find('TD:eq(5)').text(),
+            "Summary": $(tr).find('TD:eq(6)').text(),
+            "Posted": $(tr).find('TD:eq(7)').text(),
+            "Images": $(tr).find('TD:eq(8)').text(),
+          }
+        });
+        //Convert the tableData array to a JSON object
+        const janData = JSON.stringify(tableData1);
+        //janData is our table turned into a JSON object
+        // log janData to confirm output
+        console.log(janData);
+    
+      });
 
-    var $th = $(jan23,'table th');
-    $(jan23,'table tbody tr').each(function(i, tr){
-        var obj = {}, $tds = $(tr).find('td');
-        $th.each(function(index, th){
-        obj[$(th).text()] = $tds.eq(index).text();
-    });
-    myRows.myRows.push(obj);
-});
-    alert(JSON.stringify(myRows));
-    $('#out-put').html(JSON.stringify(myRows));
-       
-});
-// tests for pulling the data
-// $.get('https://nuforc.org/webreports/ndxe202302.html', function(feb23) {
-//     console.log(feb23);
-// });
-// $.get('https://nuforc.org/webreports/ndxe202303.html', function(mar23) {
-//     console.log(mar23);
-// });
+//get the data from the site for february 2023
+$.get('https://nuforc.org/webreports/ndxe202302.html', function(feb23) {
+      //create an array to store the table data
+         const tableData2 = [];
+    //iterate through the table data by each row and separate out the td 
+         $(feb23).find('tr').each(function(row, tr) {
+           
+          tableData2[row] = {
+            "Date": $(tr).find('TD:eq(0)').text(),
+            "City": $(tr).find('TD:eq(1)').text(),
+            "State": $(tr).find('TD:eq(2)').text(),
+            "County": $(tr).find('TD:eq(3)').text(),
+            "Shape": $(tr).find('TD:eq(4)').text(),
+            "Duration": $(tr).find('TD:eq(5)').text(),
+            "Summary": $(tr).find('TD:eq(6)').text(),
+            "Posted": $(tr).find('TD:eq(7)').text(),
+            "Images": $(tr).find('TD:eq(8)').text(),
+          }
+        });
+        //Convert the tableData array to a JSON object
+        const febData = JSON.stringify(tableData2);
+        //febData is our table turned into a JSON object
+        // log febData to confirm output
+        console.log(febData);
+    
+      });
 
-// var AoA = $('table tr').map(function(){
-//     return [
-//         $('td',this).map(function(){
-//             return $(this).text();
-//         }).get()
-//     ];
-// }).get();
-// var json = JSON.stringify(AoA);
+      //get the data from the site for march 2023
+$.get('https://nuforc.org/webreports/ndxe202303.html', function(mar23) {
+      //create an array to store the table data
+         const tableData3 = [];
+    //iterate through the table data by each row and separate out the td 
+         $(mar23).find('tr').each(function(row, tr) {
+           
+          tableData3[row] = {
+            "Date": $(tr).find('TD:eq(0)').text(),
+            "City": $(tr).find('TD:eq(1)').text(),
+            "State": $(tr).find('TD:eq(2)').text(),
+            "County": $(tr).find('TD:eq(3)').text(),
+            "Shape": $(tr).find('TD:eq(4)').text(),
+            "Duration": $(tr).find('TD:eq(5)').text(),
+            "Summary": $(tr).find('TD:eq(6)').text(),
+            "Posted": $(tr).find('TD:eq(7)').text(),
+            "Images": $(tr).find('TD:eq(8)').text(),
+          }
+        });
+        //Convert the tableData array to a JSON object
+        const marData = JSON.stringify(tableData3);
+        //marData is our table turned into a JSON object
+        // log marData to confirm output
+        console.log(marData);
+    
+      });
+ 
+     
+ 
+
+
 
 
 
