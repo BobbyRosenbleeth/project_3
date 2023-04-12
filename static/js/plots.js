9d74a634ff8a761bbca717ee368532d2b8667c92
chanahdunbar <dunbarchanah@gmail.com>
2023-04-11 22:43:51 -0400

    Plotly Commit

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,5 +1,5 @@
 // Create bar chart
-//var myBarChart = ('barChart', {zoom: 2})
+// var myBarChart = ('barChart', {zoom: 2})
 
 // Connect
 d3.json('/statedata2').then(function(data){console.log(data);})  
@@ -9,13 +9,14 @@ let states = data.states
 let stateCounts = data.counts
 
 // plot data into bar chart
-//create BarChart
+// create BarChart
 // May or may not use this: function BuildChart(states, counts)
+// Show the top 3 states for each month
 function init() {
   var trace1 = {
-      x: states.states,//.slice(0,4).reverse(),
-      y: stateCounts.counts,//.slice(0,4).reverse(),
-      text: states.states,//.slice(0,4).reverse(),
+      x: states.slice(0,4),
+      y: stateCounts,//.slice(0,4),
+      text: states.slice(0,4),
       type: "bar",
       orientation: "h"
   };
@@ -29,40 +30,43 @@ function init() {
   Plotly.newPlot('plot', data, layout);
 }
 
+
+//Dropdown Menu
+
 // Call updatePlotly() when a change takes place to the DOM
-//d3.selectAll("#month-select").on("change", updatePlotly);
+d3.selectAll("#month-select").on("change", updatePlotly);
 
 // This function is called when a dropdown menu item is selected
-//function updatePlotly() {
+function updatePlotly() {
   // Use D3 to select the dropdown menu
-  //let dropdownMenu = d3.select("#month-select");
+  let dropdownMenu = d3.select("#month-select");
   // Assign the value of the dropdown menu option to a variable
-  //let month = dropdownMenu.property("value");
+  let month = dropdownMenu.property("value");
 
   // Initialize x and y arrays
-  //let x = [];
-  //let y = [];
+  let x = [];
+  let y = [];
 
- // if (month === '01') {
- //   x = states.slice(0,4).reverse(),
- //   y = stateCounts;
- // }
+  if (month === 'January') {
+    x = states.slice(0,4),
+    y = stateCounts;
+  }
 
-//  else if (month === '02') {
-//    x = states.slice(0,4).reverse(),
-//    y = stateCounts;
-// }
- // else if (month ==='03') {
- //   x = states.slice(0,4).reverse(),
- //   y = stateCounts;
- // }
+  else if (month === 'February') {
+    x = states.slice(0,4),
+    y = stateCounts;
+ }
+  else if (month ==='March') {
+    x = states.slice(0,4),
+    y = stateCounts;
+  }
 
   // Note the extra brackets around 'x' and 'y'
-//  Plotly.restyle("plot", "x", [x]);
-//  Plotly.restyle("plot", "y", [y]);
-//}
+  Plotly.restyle("plot", "x", [x]);
+  Plotly.restyle("plot", "y", [y]);
+}
 
-//init();
+init();
 
 //function init() {
   //d3.json("/statedata2").then(function (data) {
