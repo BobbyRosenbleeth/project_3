7fbc37237d438c0e0c1d6677fbc57e7969e70e1e
Sahmirah <118565186+Sahmirah@users.noreply.github.com>
2023-04-13 13:18:46 -0700

    Updating Files before pulling

--- static/js/plots.js
+++ static/js/plots.js
@@ -33,57 +33,57 @@ function init() {
 }
 
 // Call updatePlotly() when a change takes place to the DOM
-//d3.selectAll("#month-select").on("change", updatePlotly);
+d3.selectAll("#month-select").on("change", updatePlotly);
 
 // This function is called when a dropdown menu item is selected
-//function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  //let dropdownMenu = d3.select("#month-select");
-  // Assign the value of the dropdown menu option to a variable
-  //let month = dropdownMenu.property("value");
+function updatePlotly() {
+  //Use D3 to select the dropdown menu
+  let dropdownMenu = d3.select("#month-select");
+  //Assign the value of the dropdown menu option to a variable
+  let month = dropdownMenu.property("value");
 
-  // Initialize x and y arrays
-  //let x = [];
-  //let y = [];
+  //Initialize x and y arrays
+  let x = [];
+  let y = [];
 
- // if (month === '01') {
- //   x = states.slice(0,4).reverse(),
- //   y = stateCounts;
- // }
+ if (month === '01') {
+   x = states.slice(0,4).reverse(),
+   y = stateCounts;
+ }
 
-//  else if (month === '02') {
-//    x = states.slice(0,4).reverse(),
-//    y = stateCounts;
-// }
- // else if (month ==='03') {
- //   x = states.slice(0,4).reverse(),
- //   y = stateCounts;
- // }
+ else if (month === '02') {
+   x = states.slice(0,4).reverse(),
+   y = stateCounts;
+}
+ else if (month ==='03') {
+   x = states.slice(0,4).reverse(),
+   y = stateCounts;
+ }
 
   // Note the extra brackets around 'x' and 'y'
-//  Plotly.restyle("plot", "x", [x]);
-//  Plotly.restyle("plot", "y", [y]);
-//}
+ Plotly.restyle("plot", "x", [x]);
+ Plotly.restyle("plot", "y", [y]);
+}
 
-//init();
+init();
 
-//function init() {
-  //d3.json("/statedata2").then(function (data) {
-      // Set up the DropDown:
-      //const month = data.date;
-      //let DropDown = d3.select(`#month-select`);
-      //month.forEach((date) => {
-       // DropDown.append(`option`).text(date).property(`value`, date);
-      //});
-      //// Reset demographic info and visuals to first subject when page is refreshed.
-      //const first = month[0];
-   // BuildChart(first);
- // });
-//}
-   // Pull data for new subject into demo and visuals.
-//function optionChanged(newMonth) {
-  //BuildChart(newMonth);
-//}
+function init() {
+  d3.json("/statedata2").then(function (data) {
+      Set up the DropDown:
+      const month = data.date;
+      let DropDown = d3.select(`#month-select`);
+      month.forEach((date) => {
+       DropDown.append(`option`).text(date).property(`value`, date);
+      });
+      // Reset demographic info and visuals to first subject when page is refreshed.
+      const first = month[0];
+ BuildChart(first);
+  });
+}
+//Pull data for new subject into demo and visuals.
+function optionChanged(newMonth) {
+  BuildChart(newMonth);
+}
 
-//init();
+init();
 
